54d259626bae0b1dea4858add7aed6fc04a943ab
Yoann Braie <yoann@yoannbraie.fr>
2021-03-06 08:11:17 +0100

    feat: init cv page

---
+++ components/blocs/CvBlock.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react'
+import styles from '../../styles/Cv.module.css'
+
+export default function CvBlock(props) {
+    
+    let data = props.data ?? null;
+
+    return (
+        <div className={styles.cvBlock}>
+            aya
+        </div>
+    )
+}
--- pages/cv.js
+++ pages/cv.js
@@ -1,13 +1,16 @@
 import Head from 'next/head'
+import CvBlock from '../components/blocs/CvBlock';
 import Layout from '../components/Layout'
 import Section from '../components/Section'
 import styles from '../styles/Cv.module.css'
+import globalStyles from '../styles/Globals.module.css';
 
 export default function Cv() {
   return (
     <Layout titlePage="Mon CV">
       <Section>
-        <h1>Mon CV</h1>
+        <h1 className={globalStyles.globalh1}>Mon CV</h1>
+        <CvBlock></CvBlock>
       </Section>
     </Layout>
   )
